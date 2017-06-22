5f3da5027e03e36e38876d3be15bb20b648d21d1
Marcin Paćko <pacek253@gmail.com>
2017-06-22 19:30:29 +0200

    implementacja GUI

--- src/federacje/GUI/GUIFederate.java
+++ src/federacje/GUI/GUIFederate.java
@@ -1,7 +1,26 @@
 package federacje.GUI;
 
+import common.FederateAbstract;
+import objects.Kasa;
+
+import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Marcin on 22.06.2017.
  */
-public class GUIFederate {
+public class GUIFederate extends FederateAbstract {
+    public GUIapp GUI;
+    public Lock CheckStartedLock = new ReentrantLock();
+    public boolean Started=false;
+    public LinkedList<Kasa> kasy = new LinkedList<>();
+    public GUIFederate(GUIapp GUI)
+    {
+        this.GUI=GUI;
+    }
+    public void runFederate(){
+
+    }
+
 }
--- src/federacje/GUI/GUIapp.java
+++ src/federacje/GUI/GUIapp.java
@@ -1,14 +1,139 @@
 package federacje.GUI;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import objects.Kasa;
+
+import java.io.PrintStream;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by Marcin on 22.06.2017.
  */
 public class GUIapp extends Application {
+
+    GUIFederate federate;
+    Button StartSym;
+    Button StopSym;
+    public static String GuiLog="GUIFaderate:";
+
+    public ListView<String> IPLIst= new ListView<>();
+    TextArea Konsola= new TextArea("Brak wpisow");
+    public PrintStream ps;
+
+    public void StopSetHendler(Stage primaryStage)
+    {
+        StopSym.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+
+               federate.CheckStartedLock.lock();
+                federate.Started=false;
+                federate.CheckStartedLock.unlock();
+                StopSym.setVisible(false);
+            }
+        });
+    }
+    public void StartSetHendler(Stage primaryStage)
+    {
+        StartSym.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                federate.CheckStartedLock.lock();
+                federate.Started=true;
+                federate.CheckStartedLock.unlock();
+
+                StopSym.setVisible(true);
+                StartSym.setVisible(false);
+            }
+        });
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
+        federate=new GUIFederate(this);
+        setLeyout(primaryStage);
+        StartSetHendler(primaryStage);
+        StopSetHendler(primaryStage);
+        //  GUIFederate.SrunFederate(this);
+        Thread thread = new Thread() {
+            public void run() {
+                federate.runFederate();
 
+            }
+
+        };
+        thread.start();
+
+
+    }
+
+    public void setLeyout(Stage primaryStage)
+    {
+        primaryStage.setTitle("GUI Federate");
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+        StartSym = new Button();
+        StopSym = new Button();
+        StopSym.setText("Zakoncz Symulacje");
+        StartSym.setText("Rozpocznij Symulacje");
+        StopSetHendler(primaryStage);
+        StartSetHendler(primaryStage);
+        StackPane root = new StackPane();
+        GridPane grid = new GridPane();
+
+        grid.add(StartSym,1,5);
+        grid.add(StopSym,0,5);
+        IPLIst.setMinSize(20,120);
+        grid.add(IPLIst, 0, 6, 2, 6);
+        grid.add(Konsola,0,30,2,30);
+        primaryStage.setScene(new Scene(root, 500, 650));
+        root.getChildren().add(grid);
+        StopSym.setVisible(false);
+        StartSym.setVisible(true);
+        primaryStage.show();
+        rewriteControls();
+    }
+    public void rewriteControls()
+    {
+        Thread tr= new Thread() {
+            public void run() {
+
+                while (true) {
+                    Platform.runLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            ObservableList<String> items = FXCollections.observableArrayList(Kasa.ToStringList(federate.kasy));
+                            IPLIst.setItems(items);
+                            Konsola.setText(GuiLog);
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        tr.start();
+
+    }
+    @Override
+    public void stop() {
+        System.out.println("Closing app");
+        System.exit(0);
     }
 }
