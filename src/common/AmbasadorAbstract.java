57ac7f590970922feb9ddf9e1088f10c569da6a2
Marcin Paćko <pacek253@gmail.com>
2017-06-23 12:37:30 +0200

    Dodanie external event dla obiektow

--- src/common/AmbasadorAbstract.java
+++ src/common/AmbasadorAbstract.java
@@ -1,9 +1,14 @@
 package common;
 
-import hla.rti.LogicalTime;
-import hla.rti.RTIambassador;
+import hla.rti.*;
+import hla.rti.jlc.EncodingHelpers;
 import hla.rti.jlc.NullFederateAmbassador;
+import objects.Kasa;
+import objects.Klient;
 import org.portico.impl.hla13.types.DoubleTime;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Marcin on 22.06.2017.
@@ -23,7 +28,8 @@ public class AmbasadorAbstract extends NullFederateAmbassador {
     public boolean isAdvancing = false;
     public boolean isRunning=true;
 
-
+    protected ArrayList<ExternalEventAbstract> externalEvents = new ArrayList<>();
+    private java.util.List<Map.Entry<String, Integer>> pairList = new java.util.ArrayList<>();
     private static void log (String x){
         System.out.println(x);
 
@@ -94,9 +100,92 @@ public class AmbasadorAbstract extends NullFederateAmbassador {
         this.federateTime = convertTime(theTime);
         this.isAdvancing = false;
     }
+    public static boolean intequalifnullfalse(int one,int two)
+    {
+     //   if(one==null) return false;
+      //  if(two==null) return false;
+        //#TODO dokonczyc implementacje
+        return false;
+    }
+    public void receiveInteraction(int interactionClass,
+                                   ReceivedInteraction theInteraction,
+                                   byte[] tag,
+                                   LogicalTime theTime,
+                                   EventRetractionHandle eventRetractionHandle) {
+        StringBuilder builder = new StringBuilder("Interaction Received:");
 
+
+        if(interactionClass ==subskrypcje.rozpoczecieSymulacjiHandler.getRozpoczecieSymulacjiHandler()) {
+
+            builder.append("Start Symulacji z gui\n");
+            isRunning=true;
+
+        }
+        if(interactionClass ==subskrypcje.zakonczenieSymulacjiHandler.getZakonczenieSymulacjiHandler()) {
+
+            builder.append("Zakonczenie symulacji z gui\n");
+            isRunning=false;
+        }
+        log(builder.toString());
+        //#TODO
+    }
     protected double convertTime(LogicalTime logicalTime) {
         return ((DoubleTime) logicalTime).getTime();
     }
+    public void reflectAttributeValues(int theObject,
+                                       ReflectedAttributes theAttributes,
+                                       byte[] tag,
+                                       LogicalTime theTime,
+                                       EventRetractionHandle retractionHandle) {
+        StringBuilder builder = new StringBuilder("Reflection for object: ");
+        builder.append(" handle=" + theObject);
+        builder.append(", tag=" + EncodingHelpers.decodeString(tag));
 
+        if (theTime != null)
+            builder.append(", time=" + convertTime(theTime));
+        builder.append(", attributeCount=" + theAttributes.size());
+        builder.append("\n");
+        for (Map.Entry<String, Integer> m : pairList) {
+            if (m.getValue() == theObject) {
+                double time = convertTime(theTime);
+                ExternalEventAbstract externalEvent = new ExternalEventAbstract();
+                if (m.getKey().contains("Kasa")) {
+                    try {
+                        Kasa kasa = new Kasa();
+                        int size = theAttributes.size();
+                        kasa.NumerKasy = EncodingHelpers.decodeInt(theAttributes.getValue(1));
+                        kasa.Dlugosc = EncodingHelpers.decodeInt(theAttributes.getValue(2));
+                        kasa.CzyOtwarta = EncodingHelpers.decodeBoolean(theAttributes.getValue(0));
+                        kasa.CzyPelna = EncodingHelpers.decodeBoolean(theAttributes.getValue(3));
+
+                        externalEvent.KasaEvent(kasa, time);
+                        this.externalEvents.add(externalEvent);
+                    } catch (RTIexception aioob) {
+                        //
+                    }
+
+                } else if (m.getKey().contains("Klient")) {
+                    try {
+                        Klient klient = new Klient();
+
+                        int size = theAttributes.size();
+                        klient.IDKlienta = EncodingHelpers.decodeInt(theAttributes.getValue(0));
+                        klient.uprzywilejowany = EncodingHelpers.decodeBoolean(theAttributes.getValue(1));
+                        klient.NumerKolejki = EncodingHelpers.decodeInt(theAttributes.getValue(2));
+                        klient.NumerWKolejce = EncodingHelpers.decodeInt(theAttributes.getValue(3));
+                        klient.Gotowka = EncodingHelpers.decodeInt(theAttributes.getValue(4));
+
+                        // System.out.println("jest: "+klient);
+                        externalEvent.KlientEvent(klient, time);
+                        this.externalEvents.add(externalEvent);
+                    } catch (RTIexception aioob) {
+                        //
+                    }
+                }
+            }
+            log("" + builder);
+        }
+
+
+    }
 }
--- src/common/ExternalEventAbstract.java
+++ src/common/ExternalEventAbstract.java
@@ -1,7 +1,63 @@
 package common;
 
+import objects.Kasa;
+import objects.Klient;
+
+import java.util.Comparator;
+
 /**
  * Created by Marcin on 22.06.2017.
  */
 public class ExternalEventAbstract {
+    public enum EventType {Kasa, Klient}
+
+    private Klient k;
+    private Kasa kas;
+
+    private EventType eventType;
+    private Double time;
+
+
+    public void KasaEvent(Kasa kas, Double time) {
+        this.kas = kas;
+        this.eventType = EventType.Kasa;
+        this.time = time;
+    }
+    public void KlientEvent(Klient k, EventType eventType, Double time) {
+        this.k = k;
+        this.eventType = eventType;
+        this.time = time;
+    }
+    public void KlientEvent(Klient k, Double time) {
+        this.k = k;
+        this.eventType = EventType.Klient;
+        this.time = time;
+    }
+    public void KlientEvent(int IDKlienta,boolean uprzywilejowany, int NumerKolejki,int NumerWKolejce,int Gotowka, EventType eventType, Double time) {
+        this.k = new Klient(IDKlienta,uprzywilejowany,NumerKolejki,NumerWKolejce,Gotowka);
+        this.eventType = eventType;
+        this.time = time;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public Klient getKlient()
+    {
+        return k;
+    }
+
+    public double getTime() {
+        return time;
+    }
+    public Kasa getKasa() {return kas;}
+    public static class ExternalEventComparator implements Comparator<ExternalEventAbstract> {
+
+        @Override
+        public int compare(ExternalEventAbstract o1, ExternalEventAbstract o2) {
+            return o1.time.compareTo(o2.time);
+        }
+    }
 }
+
