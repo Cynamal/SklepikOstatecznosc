3c2751c200185b317334c88372a85ffd6c0e25f2
Marcin Paćko <pacek253@gmail.com>
2017-06-25 10:54:57 +0200

    Dodanie bibliotek excelowych

---
+++ src/statistic/ExcelExporter/Exporter.java
@@ -0,0 +1,7 @@
+package statistic.ExcelExporter;
+
+/**
+ * Created by Marcin on 25.06.2017.
+ */
+public class Exporter {
+}
---
+++ src/statistic/ExcelExporter/ExporttableList.java
@@ -0,0 +1,12 @@
+package statistic.ExcelExporter;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Marcin on 25.06.2017.
+ */
+public abstract class ExporttableList extends LinkedList{
+    LinkedList<String> hedders=new LinkedList<>(); //hedders for data or calculated walues
+
+
+}
